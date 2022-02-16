c903cfd985444f8d7e23ca109180b75a6f440662
oliasugak <50302746+oliasugak@users.noreply.github.com>
2022-02-16 23:50:20 +0200

    Add files via upload

---
+++ lab2/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const num = 2;
+
+ReactDOM.render(
+
+  <h1>{num} lab
+  <p>Olia and Alina</p></h1>,
+  document.getElementById("root"),
+);
